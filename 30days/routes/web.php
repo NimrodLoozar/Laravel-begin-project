e37a0e848a9cf36eb0fa2e6cc14450206040df6d
NimrodLobozar <nimrod.lobozar@gmail.com>
2024-09-13 14:04:07 +0200

    nieuwe dashboard

--- 30days/routes/web.php
+++ 30days/routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
 });
