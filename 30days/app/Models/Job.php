c717521fbca86a5b4e0f5dcd0db42792f87fab0f
NimrodLobozar <nimrod.lobozar@gmail.com>
2024-09-30 10:56:04 +0200

    day 9

--- 30days/app/Models/Job.php
+++ 30days/app/Models/Job.php
@@ -2,37 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
+    protected $table = 'job_listings';
 
-    public static function find(int $id)
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-    }
+    protected $fillable = ['title', 'salary'];
 }
