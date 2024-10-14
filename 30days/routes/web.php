142ce26ec6fff01c4d721d1f85f239ec918a6031
NimrodLobozar <nimrod.lobozar@gmail.com>
2024-10-14 11:26:16 +0200

    les 17

--- 30days/routes/web.php
+++ 30days/routes/web.php
@@ -32,6 +32,11 @@ Route::get('/jobs/{id}', function ($id) {
 Route::post('/jobs', function () {
     //Validation
     // dd('helo form the post request');
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
